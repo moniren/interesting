398f4278e1a5626874bcba33327555f9c383066d
ykli <390769017qq@gmail.com>
2015-11-14 00:30:34 +0800

    compressed the code

--- friday_13/friday_13.c
+++ friday_13/friday_13.c
@@ -1,14 +1,28 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+int year=2015;
+int month=11;
+int limit;
+int hits = 0;
+int loop_counter = 0;
+int days = 0;
+
+
+void verify_date(){
+    month = (month + 1) % 12;
+    if(days % 7 == 0){
+      printf("Year %d month %d\n",year,month);
+      days = 0;
+      hits++;
+      if(hits == limit)
+        exit(0);
+    }
+}
+
 
 int main(){
 
-    int year=2015;
-    int month=11;
-    int limit;
-    int hits = 0;
-    int loop_counter = 0;
-    int days = 0;
-    
     printf("How many predictions do you want?\n");
     scanf("%d",&limit);
 
@@ -19,127 +33,44 @@ int main(){
       while(1){
         //+nov - now dec
         days += 30;
-        month = 12;
-        if(days % 7 == 0){
-          printf("Year %d month %d\n",year,month);
-          days = 0;
-          hits++;
-          if(hits == limit)
-            return 0;
-        }
+        verify_date();
         //+dec - now jan
         year++;
         days += 31;
-        month = 1;
-        if(days % 7 == 0){
-          printf("Year %d month %d\n",year,month);
-          days = 0;
-          hits++;
-          if(hits == limit)
-            return 0;
-        }
+        verify_date();
         //+jan - now feb
         days += 31;
-        month = 2;
-        if(days % 7 == 0){
-          printf("Year %d month %d\n",year,month);
-          days = 0;
-          hits++;
-          if(hits == limit)
-            return 0;
-        }
+        verify_date();
         //+feb - now mar
         if(year % 4 == 0)
           days += 29;
         else
           days += 28;
-        month = 3;
-        if(days % 7 == 0){
-          printf("Year %d month %d\n",year,month);
-          days = 0;
-          hits++;
-          if(hits == limit)
-            return 0;
-        }
+        verify_date();
         //+mar - now apr
         days += 31;
-        month = 4;
-        if(days % 7 == 0){
-          printf("Year %d month %d\n",year,month);
-          days = 0;
-          hits++;
-          if(hits == limit)
-            return 0;
-        }
+        verify_date();
         //+apr - now may
         days += 30;
-        month = 5;
-        if(days % 7 == 0){
-          printf("Year %d month %d\n",year,month);
-          days = 0;
-          hits++;
-          if(hits == limit)
-            return 0;
-        }
+        verify_date();
         //+may - now jun
         days += 31;
-        month = 6;
-        if(days % 7 == 0){
-          printf("Year %d month %d\n",year,month);
-          days = 0;
-        }
+        verify_date();
         //+jun - now jul
         days += 30;
-        month = 7;
-        if(days % 7 == 0){
-          printf("Year %d month %d\n",year,month);
-          days = 0;
-          hits++;
-          if(hits == limit)
-            return 0;
-        }
+        verify_date();
         //+jul - now aug
         days += 31;
-        month = 8;
-        if(days % 7 == 0){
-          printf("Year %d month %d\n",year,month);
-          days = 0;
-          hits++;
-          if(hits == limit)
-            return 0;
-        }
+        verify_date();
         //+aug - now sep
         days += 31;
-        month = 9;
-        if(days % 7 == 0){
-          printf("Year %d month %d\n",year,month);
-          days = 0;
-          hits++;
-          if(hits == limit)
-            return 0;
-        }
+        verify_date();
         //+sep - now oct
         days += 30;
-        month = 10;
-        if(days % 7 == 0){
-          printf("Year %d month %d\n",year,month);
-          days = 0;
-          hits++;
-          if(hits == limit)
-            return 0;
-        }
+        verify_date();
         //+oct - now nov
         days += 31;
-        month = 11;
-        if(days % 7 == 0){
-          printf("Year %d month %d\n",year,month);
-          days = 0;
-          hits++;
-          if(hits == limit)
-            return 0;
-        }
-
-
+        verify_date();
       }
 
     }
